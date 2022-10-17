b86e8f8ae1f9e3690314049897f1d8181b15c0f0
Stephan Baumann <stephan.baumann@students.fhnw.ch>
2022-10-17 12:50:54 +0200

    two versions

--- 06_Recursion/Lecture/ABRecusion.hs
+++ 06_Recursion/Lecture/ABRecusion.hs
@@ -1,5 +1,11 @@
 -- Aufgabe 2
 
 countDown :: Int -> [Int]
-countDown 0 = [0]
-countDown n = [n] ++ countDown (n-1)
+countDown n
+    | n == 0 = [0]
+    | n > 0 = [n] ++ countDown (n-1)
+
+
+-- oder
+-- countDown 0 = [0]
+-- countDown n = [n] ++ countDown (n-1)
