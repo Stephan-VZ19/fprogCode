7f273da3dcba8741e90c622d36779fe334c6c669
Stephan Baumann <stephan.baumann@students.fhnw.ch>
2022-10-21 17:04:08 +0200

    Uebung Rekursion Aufgabe 1

---
+++ 06_Recursion/Assignment/AS_Recursion.hs
@@ -0,0 +1,13 @@
+-- Ubung: Rekursion
+
+-- Aufgabe 1
+-- a)
+
+max' :: (Ord a) => a -> a -> a
+max' x y
+    | x >= y = x
+    | otherwise = y
+
+maxl :: (Ord a) => [a] -> a
+maxl (x:y:[]) = max' x y
+maxl (x:y:xs) = maxl ((max' x y):xs)
