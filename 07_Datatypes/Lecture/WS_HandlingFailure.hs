fde4f87ae9b8666ea7390585d768044550c2a690
Stephan Baumann <stephan.baumann@students.fhnw.ch>
2022-11-04 19:30:23 +0100

    WS done, without correction

--- 07_Datatypes/Lecture/WS_HandlingFailure.hs
+++ 07_Datatypes/Lecture/WS_HandlingFailure.hs
@@ -6,3 +6,19 @@ safeHead (x:xs) = Just x
 
 -- b)
 
+safeTail :: [a] -> Maybe [a]
+safeTail [] = Nothing
+safeTail [_] = Nothing
+safeTail (x:xs) = Just xs
+
+-- c)
+
+safeMax :: (Ord a) => [a] -> Maybe a
+safeMax [] = Nothing
+safeMax [a] = Just a
+safeMax [a, b]
+    | a > b = Just a
+    | otherwise = Just b
+safeMax (x:y:xs)
+    | x > y = Just safeMax (x:xs)
+    | otherwise = Just safeMax (y:xs)
