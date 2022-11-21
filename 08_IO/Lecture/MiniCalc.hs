a48b378508362c96c565b821557dd84532e8e221
Stephan Baumann <stephan.baumann@students.fhnw.ch>
2022-11-21 14:36:12 +0100

    MiniCalc working

--- 08_IO/Lecture/MiniCalc.hs
+++ 08_IO/Lecture/MiniCalc.hs
@@ -1,8 +1,11 @@
 main :: IO ()
-main = do putStrLn "Please enter a number to be added:"
-          number <- getLine
+main = do putStrLn "Welcome to Minicalc!"
+          putStrLn "Please enter a number to be added:"
+          num1 <- getLine
+          let num11 = read num1 :: Int
           putStrLn "Please enter a second number to be added:"
           num2 <- getLine
-          let msg = "Sum = "
+          let num22 = read num2 :: Int
+          let res = show (num11 + num22)
+          let msg = "Sum = " ++ num1 ++ " + " ++ num2 ++ " = " ++ res
           putStrLn msg
-          putStrLn (number + num2)
