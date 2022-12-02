c03b8c2fa9297906382030aa77c2ed12f9b95bd3
Stephan Baumann <stephan.baumann@students.fhnw.ch>
2022-12-02 21:14:38 +0100

    IO

--- 08_IO/Lecture/WS_Webclient/Main.hs
+++ 08_IO/Lecture/WS_Webclient/Main.hs
@@ -11,4 +11,18 @@ import qualified Data.ByteString.Lazy.Char8 as L8
 -- cabal run webclient Arg1
 main :: IO ()
 main = do
+    manager <- newManager ...
+    let manager2 = read manager :: Manager
+    let urlA = "https://wttr.in/"
+    let urlB = "?format=3"
+    putStrLn "Please enter the location:"
+    location <- getLine
+    let loc = read location :: String
+    let url = urlA ++ loc ++ urlB
+    request <- parseRequest url
+
+
+
+
+
     putStrLn "Load the current weather from https://wttr.in"
